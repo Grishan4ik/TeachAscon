b10cf2ee9fbf3b37319bc0a1a5cec3d4be2b2666
Григорий <Grishan4ik635@gmail.com>
2024-09-24 11:45:38 +0400

    Add project files.

---
+++ ConsoleAppAscon2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.Write("Введите первое значение");
+string? FirstNumb = Console.ReadLine();
+int FirstNumber = int.Parse(FirstNumb);
+Console.Write("Введите второе значение");
+string? SecondNumb = Console.ReadLine();
+int SecondNumber = int.Parse(FirstNumb);
+
+Console.Write("Укажите математическое действие");
+string? MathSumbol = Console.ReadLine();
+
+//string Math = "+ - * /";
+//int MathNumber = int.Parse(Math);
+switch (MathSumbol)
+{
+    case "+":
+        Console.WriteLine($" {FirstNumber + SecondNumber}");
+        break;
+    case "-":
+        Console.WriteLine($" {FirstNumber} - {SecondNumber}");
+        break;
+    case "*":
+        Console.WriteLine($" {FirstNumber} * {SecondNumber}");
+        break;
+    case "/":
+        Console.WriteLine($" {FirstNumber} / {SecondNumber}");
+        break;
+}
+
